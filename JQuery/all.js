6195d7487dd92c2fe19b3c0bca5112023836c307
roceil <a0978006326@gmail.com>
2022-06-14 14:05:48 +0800

    履歷完成版V1.0
    含scss/jquery

---
+++ JQuery/all.js
@@ -0,0 +1,6 @@
+    $(document).ready(function () {
+        $('.btn-top').click(function(){
+            $('html,body').animate({ scrollTop: 0 }, 'slow');
+            return false;
+        });
+    });
